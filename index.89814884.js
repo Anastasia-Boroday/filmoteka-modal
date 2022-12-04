28d9c62629027b39a76e41a1064f0e06fe9fed81
Anastasia-Boroday <93268396+Anastasia-Boroday@users.noreply.github.com>
2022-12-04 02:38:11 +0000

    Deploying to gh-pages from @ Anastasia-Boroday/filmoteka-modal@cd6686902ae184e8271e9a27dc94dbce9102631d 🚀

---
+++ index.89814884.js
@@ -0,0 +1,2 @@
+!function(){var e;(e={openModalBtn:document.querySelector("[data-modal-open]"),modal:document.querySelector("[data-modal]")}).openModalBtn.addEventListener("click",(function(){e.modal.classList.toggle("is-hidden")}))}();
+//# sourceMappingURL=index.89814884.js.map
